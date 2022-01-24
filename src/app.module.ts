41f37a606ef5f9022dbbb27b848f74f670c0eae3
Roman Montoya <romanmontoya@Romans-MacBook-Pro-2.local>
2022-01-24 16:44:58 +0900

    feat: get directions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DirectionsModule } from './directions/directions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [DirectionsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
