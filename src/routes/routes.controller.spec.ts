63f2186cbc2782768bff09e1c3842f04291e660f
Roman Montoya <romanmontoya@Romans-MacBook-Pro-2.local>
2022-02-01 11:45:38 +0900

    test: route controller get request

--- src/routes/routes.controller.spec.ts
+++ src/routes/routes.controller.spec.ts
@@ -1,20 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoutesController } from './routes.controller';
 import { RoutesService } from './routes.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 describe('RoutesController', () => {
-  let controller: RoutesController;
+  let routesController: RoutesController;
+  let routesService: RoutesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RoutesController],
-      providers: [RoutesService],
+      providers: [RoutesService, {
+        provide: getModelToken('Route'),
+        useValue: {
+          save: jest.fn().mockResolvedValue(Model),
+          find: jest.fn().mockResolvedValue([Model])
+        },
+      }],
     }).compile();
 
-    controller = module.get<RoutesController>(RoutesController);
+    routesController = module.get<RoutesController>(RoutesController);
+    routesService = module.get<RoutesService>(RoutesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe.only('findAll', () => {
+    it('should return an array', async () => {
+      const result = [];
+      jest.spyOn(routesService, 'findAll').mockImplementation(async () => result);
+      expect(await routesController.findAll()).toBe(result);
+    });
   });
 });
